d1aa640b810eefccef4a68a03ee9d220c39b4e07
Cynthia <vaj.cynthia@gmail.com>
2025-11-13 00:27:29 -0600

    Commit 2- Part 4 Optional Goal 1

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -22,8 +23,24 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: { 
+      react: { 
+        version: 'detect' 
+      } 
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
     rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      //...other rules
+        'no-unused-vars': 'warn', //this changes the error to a warning
+        'react/prop-types': 'off', //this suppresses warnings about not using prop-types
+        //other rules...
     },
   },
 ])
