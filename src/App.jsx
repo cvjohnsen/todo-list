84dac10407d2380ba432217cd5e48366280102bf
Cynthia <vaj.cynthia@gmail.com>
2025-11-19 18:03:49 -0600

    Commit 2- Part 2: TodoForm Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import TodoList from './TodoList'
+import TodoForm from './TodoForm'
 
 function App() {
 
   return (
     <div>
       <h1>My Todos</h1>
+      <TodoForm/>
       <TodoList/>
-    
     </div>
   )
 }
---
+++ src/TodoForm.jsx
@@ -0,0 +1,25 @@
+function TodoForm (){
+    return (
+        <form>
+            <label htmlFor="todoTitle">Todo</label>
+            <input id="todoTitle" type="text"/>
+            <button type="button">Add Todo</button>
+        </form>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default TodoForm 
