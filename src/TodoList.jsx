1354e3e99b27c8247cfef0a1eddf330c8a3a03db
Cynthia <vaj.cynthia@gmail.com>
2026-01-02 00:27:06 -0800

    Commit 3: Part 2: Render Todos from State

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,20 +1,12 @@
 import TodoListItem from "./TodoListItem"
 
+function TodoList({todoList}){
 
-{/*extract from TodoList.jsx*/}
-function TodoList(){
-    const todos =[
-  {id: 1, title: 'review resources'},
-  {id: 2, title: 'take notes'},
-  {id: 3, title: 'code out app'},
-]
-    return (
+    return(
         <ul>
-        {todos.map((todo) => (
-          <TodoListItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
-    );
+            {todoList.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
+        </ul>
+    )
 }
 
 export default TodoList
