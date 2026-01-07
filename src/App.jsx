a6607ac1c3416e8c4663ef80f976333e8d766697
Cynthia <vaj.cynthia@gmail.com>
2026-01-06 21:24:12 -0600

    Commit 2: Part 2 - Mark Todo as Complete

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,26 @@ function App() {
     const newTodo = {
       title:title,
       id: Date.now(),
+      isCompleted: false,
     };
 
     setTodoList([...todoList, newTodo]);
   }
+function completeTodo(id) {
+  const updateTodos = todoList.map ((todo) => {
+    if (todo.id ===id) {
+      return {...todo, isCompleted: true};
+    }
+    return todo;
+  })
+  setTodoList(updateTodos);
+}
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />    
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />    
     </div>
   );
 }
