af531ce0439d40621271320c94fc6ef9af9dbe45
Cynthia <vaj.cynthia@gmail.com>
2026-02-03 00:04:59 -0600

    Part 2: Commit 2- Fetch App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
 import TodoList from "./features/TodoList/TodoList";
 import TodoForm from "./features/TodoForm";
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+const token = `Bearer ${import.meta.env.VITE_PAT}`;
 
 function App() {
 
@@ -9,6 +12,35 @@ function App() {
 
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+  const fetchTodos = async () => {
+    setIsLoading(true);
+
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: token,
+      },
+    };
+
+    try {
+      const resp = await fetch(url, options);
+
+      if (!resp.ok) {
+        throw new Error(resp.message);
+      }
+
+    } catch (error) {
+ 
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  fetchTodos();
+}, []);
   
   function addTodo(title) {
     const newTodo = {
