f5afe8e815dc31cd25c059346b3e5abf76871ebd
Cynthia <vaj.cynthia@gmail.com>
2026-02-04 00:51:09 -0600

    Part 2: Commit 5 - View Control Forms

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import TodoList from "./features/TodoList/TodoList";
 import TodoForm from "./features/TodoForm";
 import { useEffect, useState } from 'react'
-import TodosViewForm from "./features/TodosViewForm";
+import TodosViewForm from "./features/TodosViewForm.jsx";
 
 const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
 const token = `Bearer ${import.meta.env.VITE_PAT}`;
