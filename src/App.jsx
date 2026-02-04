5f55ca926860e8d5647a1a974587d5eb1d137420
Cynthia <vaj.cynthia@gmail.com>
2026-02-04 00:31:10 -0600

    Part 2: Commit 4 - View Control Forms

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import TodoList from "./features/TodoList/TodoList";
 import TodoForm from "./features/TodoForm";
 import { useEffect, useState } from 'react'
-
+import TodosViewForm from "./features/TodosViewForm";
 
 const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
 const token = `Bearer ${import.meta.env.VITE_PAT}`;
@@ -228,7 +228,8 @@ const updateTodo = async (editedTodo) => {
     
       <TodoForm onAddTodo={addTodo} isSaving={isSaving} />
       <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo} isLoading={isLoading}/>  
-
+    <hr />
+      <TodosViewForm sortDirection={sortDirection} setSortDirection={setSortDirection} sortField={sortField} setSortField={setSortField} />
       {errorMessage && (
   <div>
     <hr />
