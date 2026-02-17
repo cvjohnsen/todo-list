dcbd26ae1790c049f6e82025ae5284276c7ce815
Cynthia <vaj.cynthia@gmail.com>
2026-02-16 23:44:27 -0600

    Part 3: Duplicate State Logic to Reducer - Commit 3

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -26,59 +26,112 @@ const initialState = {
 
 function todosReducer(state = initialState, action) {
   switch (action.type) {
+   
     case actions.fetchTodos:
       return {
         ...state,
+        isLoading: true,
       };
 
-    case actions.loadTodos:
+    case actions.loadTodos: {
+      const todos = action.records.map((record) => {
+        const todo = {
+          id: record.id,
+          ...record.fields,
+        };
+
+        if (todo.isCompleted === undefined) {
+          todo.isCompleted = false;
+        }
+
+        return todo;
+      });
+
       return {
         ...state,
+        todoList: todos,
+        isLoading: false,
       };
+    }
 
     case actions.setLoadError:
       return {
         ...state,
+        errorMessage: action.error.message,
+        isLoading: false,
+        isSaving: false,
       };
 
     case actions.startRequest:
       return {
         ...state,
+        isSaving: true,
       };
 
-    case actions.addTodo:
+    case actions.addTodo: {
+      
+      const savedTodo = {
+        id: action.record.id,
+        ...action.record.fields,
+      };
+
+      if (savedTodo.isCompleted === undefined) {
+        savedTodo.isCompleted = false;
+      }
+
       return {
         ...state,
+        todoList: [...state.todoList, savedTodo],
+        isSaving: false,
       };
+    }
 
     case actions.endRequest:
       return {
         ...state,
-      };
-
-    case actions.updateTodo:
-      return {
-        ...state,
-      };
-
-    case actions.completeTodo:
-      return {
-        ...state,
+        isLoading: false,
+        isSaving: false,
       };
 
     case actions.revertTodo:
+    case actions.updateTodo: {
+      const updatedTodos = state.todoList.map((todo) =>
+        todo.id === action.editedTodo.id ? action.editedTodo : todo
+      );
+
+      const updatedState = {
+        ...state,
+        todoList: updatedTodos,
+      };
+
+      if (action.error) {
+        updatedState.errorMessage = action.error.message;
+      }
+
+      return updatedState;
+    }
+
+    case actions.completeTodo: {
+      const updatedTodos = state.todoList.map((todo) =>
+        todo.id === action.id ? { ...todo, isCompleted: true } : todo
+      );
+
       return {
         ...state,
+        todoList: updatedTodos,
       };
+    }
 
     case actions.clearError:
       return {
         ...state,
+        errorMessage: "",
       };
 
     default:
       return state;
   }
 }
+
 
 export { actions, initialState, todosReducer };
